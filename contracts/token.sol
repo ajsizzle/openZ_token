7ed9dbb0b8d46f6b44f0319243f8c9e96e2bfefd
Alex Johnson <ajohnson2389@gmail.com>
2022-02-08 11:53:38 -0500

    extending MyToken to include ERC20Capped and Ownable from OpenZeppelin

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTKN") {
+
+contract MyToken is ERC20Capped, Ownable {
+    constructor() ERC20("MyToken", "MTKN") ERC20Capped(100000) {
         _mint(msg.sender, 1000);
     }
 }
